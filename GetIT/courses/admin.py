673e90af2b87103afa3d49e0487e4a85f73ffbb9
Maki Kamiya <109022150+makikamiya@users.noreply.github.com>
2023-06-26 22:40:17 +1200

    Register Course, Chapter, Lesson models to display in admin screen
    Including customised admin displays for each model.

--- GetIT/courses/admin.py
+++ GetIT/courses/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Course, Chapter, Lesson
 
 # Register your models here.
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'language')
+    list_filter = ('language',)
+
+
+class ChapterAdmin(admin.ModelAdmin):
+    list_display = ('chapter_number', 'title', 'course')
+    list_filter = ('course',)
+    ordering = ('course', 'chapter_number')
+    list_display_links = ('chapter_number', 'title',)
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('lesson_number', 'title', 'chapter')
+    list_filter = ('chapter',)
+    ordering = ('lesson_number',)
+    list_display_links = ('lesson_number', 'title',)
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Chapter, ChapterAdmin)
+admin.site.register(Lesson, LessonAdmin)
