611c8ba922af3352db985fa683650b22c52954eb
Maki Kamiya <109022150+makikamiya@users.noreply.github.com>
2023-06-26 21:42:11 +1200

    Define Profile model in users/models.py

--- GetIT/users/models.py
+++ GetIT/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=30, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.user.username
