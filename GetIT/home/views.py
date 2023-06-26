dbecf8ca553641fca76fd1f85342e322e45a6798
Maki Kamiya <109022150+makikamiya@users.noreply.github.com>
2023-06-26 23:08:07 +1200

    Define home view to return home/welcome.html template
    Also pass today's date and current time to the template.

--- GetIT/home/views.py
+++ GetIT/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
