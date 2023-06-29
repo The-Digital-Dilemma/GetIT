fb5787e37281839689555d90f749a7af7a1d0ed3
redblindbananaz <sebhilltop@gmail.com>
2023-06-29 19:25:52 +1200

    main course link wroking

--- GetIT/courses/urls.py
+++ GetIT/courses/urls.py
@@ -4,7 +4,7 @@ from .views import CoursesListView, CourseDetailView
 # Creation of URL patterns for courses app
 
 urlpatterns = [
-    path("", CoursesListView.as_view(), name="courses"),
-    path("<int:pk>/", CourseDetailView.as_view(), name="course"),
+    path("", CoursesListView.as_view(), name="courses-list"),
+    path("<int:pk>/", CourseDetailView.as_view(), name="course-details"),
 ]
     
--- GetIT/courses/views.py
+++ GetIT/courses/views.py
@@ -6,11 +6,11 @@ from .models import Course
 # Create your views here.
 class CoursesListView(ListView):
     model = Course
-    template_name = "courses.html"
-    context_object_name = "courses"
+    template_name = "courses/courses-list.html"
+    context_object_name = "courses-list"
     
 class CourseDetailView(DetailView):
     model = Course
-    template_name = "course.html"
-    context_object_name = "course"
+    template_name = "courses/course-details.html"
+    context_object_name = "course-details"
     
