24839c82254c9016dd0cee459f2f5d58c6d9c505
redblindbananaz <sebhilltop@gmail.com>
2023-06-30 11:54:20 +1200

    Profile View creation

--- GetIT/users/views.py
+++ GetIT/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, UpdateView
+from .models import Profile
 
 # Create your views here.
+
+# Creation of the ProfileView class
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = Profile
+    template_name = "users/profile.html"
+    context_object_name = "profile"
+    
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
