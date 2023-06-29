6e9380459921a8b71d5f883943dc2c1c25daeed8
redblindbananaz <sebhilltop@gmail.com>
2023-06-30 10:56:19 +1200

    installation pillow and update requirement.txt

--- GetIT/users/models.py
+++ GetIT/users/models.py
@@ -10,6 +10,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
     email = models.EmailField(max_length=254, blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pics/", blank=True)
 
     def __str__(self):
         return self.user.username
