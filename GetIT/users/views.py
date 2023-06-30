091f151f935c97e5518bc0acfb191860b9761652
redblindbananaz <sebhilltop@gmail.com>
2023-06-30 21:16:13 +1200

    creation of form and update Views

--- GetIT/users/views.py
+++ GetIT/users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, UpdateView
 from .models import Profile
 from django.contrib.auth.models import User
+from .forms import ProfileForm
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -17,5 +19,14 @@ class ProfileView(LoginRequiredMixin, DetailView):
     def get_object(self, queryset=None):
         return self.request.user
     
-    
+# Creation of view for updating the profile
 
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    template_name = "users/profile_update.html"
+    success_url = reverse_lazy("users:profile")
+    
+    def get_object(self, queryset=None):
+        return self.request.user
+    
