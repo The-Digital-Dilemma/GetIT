d57326cd1a162138ce8bc6c6520a7bd5ff628a15
Maki Kamiya <109022150+makikamiya@users.noreply.github.com>
2023-07-01 16:17:27 +1200

    Update users/views.py for login, logout and register

--- GetIT/users/views.py
+++ GetIT/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'users/login.html'
+    next_page = 'courses_list'
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'users/logout.html'
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('home')
+        return super().get(request, *args, **kwargs)
